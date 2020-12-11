cc035e1fab1a59bbbb69fe7112fc400c6ceba5d0
Chuck Lu <chuck.lu@qq.com>
2020-12-11 14:47:35 +0800

    conditionals and loops in Vue https://vuejs.org/v2/guide/#Conditionals-and-Loops

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var app = new Vue({ 
+var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!'
     }
 });
-
 //app.message = 'I have changed the data!';
 
 var app2 = new Vue({
@@ -13,5 +12,23 @@ var app2 = new Vue({
         message: 'You loaded this page on ' + new Date().toLocaleString()
     }
 })
-
 //app2.message = 'Hello chuck';
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+});
+//app3.seen = false;
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }]
+    }
+});
+//app4.todos.push({ text: 'chuck' });
