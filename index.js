7318d3b6cf4a236484e2fdbe727699c6651e6f1d
Chuck Lu <chuck.lu@qq.com>
2020-12-11 18:07:41 +0800

    test the vue component

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+Vue.component('github-profile', {
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>',
+    data: function () {
+        return { count: 0 }
+    }
+});
+
 var app = new Vue({
     el: '#app'
 });
