67d6dbdd6d55a204c7cb4e5521db4b318ee5f367
Chuck Lu <chuck.lu@qq.com>
2021-01-11 18:47:42 +0800

    prepare for mounted test

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-Vue.component('github-profile', {
-    template: '#profile-template',
-    props:{
-        username:{
-            type: String,
-            required: true
-        }
-    },
-    data: function () {
-        return { 
-            user: {}
-         }
-    },
-    created(){
-        axios.get(`https://api.github.com/users/${this.username}`)
-        .then(response=>{
-            this.user = response.data
-        })
-    }
-});
-
 var app = new Vue({
     el: '#app',
     data:{
-        usernames:["chucklu","kerryjiang","joperezr","nkolev92"]
+        flag: true
     }
 });
